998c8e4ab70fbe4c21a67bddec9023222acb2404
Vuong Tran <minhvuong2810@gmail.com>
2022-07-04 16:13:58 +0700

    Integrate deployment configuration

--- youtube-sharing-be/src/main.ts
+++ youtube-sharing-be/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const serverPort = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log(`Server is running on http://localhost:3000`);
+  app.setGlobalPrefix('api');
+  await app.listen(serverPort);
+  console.log(`Server is running on http://localhost:${serverPort}/api`);
 }
 
 bootstrap();
