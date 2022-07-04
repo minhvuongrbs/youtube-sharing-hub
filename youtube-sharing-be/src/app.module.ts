dd7dacd2ee0905a1869823c722b26421e49dc434
Vuong Tran <minhvuong2810@gmail.com>
2022-07-04 11:03:18 +0700

    Update env reader

--- youtube-sharing-be/src/app.module.ts
+++ youtube-sharing-be/src/app.module.ts
@@ -1,18 +1,32 @@
-import { DATABASE_CONNECTION } from './constant/database';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { VideosModule } from './videos/videos.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserEntity } from './users/user.entity';
+import { VideoEntity } from './videos/video.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     AuthModule,
     UsersModule,
-    TypeOrmModule.forRoot(DATABASE_CONNECTION as TypeOrmModuleOptions),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_NAME,
+      entities: [UserEntity, VideoEntity],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
     VideosModule,
   ],
   controllers: [AppController],
--- youtube-sharing-be/src/main.ts
+++ youtube-sharing-be/src/main.ts
@@ -6,4 +6,5 @@ async function bootstrap() {
   await app.listen(3000);
   console.log(`Server is running on http://localhost:3000`);
 }
+
 bootstrap();
