0ba3a25f2e5eea3ee8dacec9a4a91e761f1b1a3e
Vuong Tran <minhvuong2810@gmail.com>
2022-07-06 00:29:56 +0700

    Fix build

--- youtube-sharing-fe/requests/userApi.ts
+++ youtube-sharing-fe/requests/userApi.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import * as Router from 'next/router';
+import Router from 'next/router';
 import { isBrowser } from '../utils/isBrowser';
 
 export const login = async ({
